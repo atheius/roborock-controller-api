181deca12831f035bd2a3a36e697a9f512701154
James Mugliston <atheiusdev@gmail.com>
2024-07-29 20:37:45 +0100

    feat: add semantic-release action (#15)

---
+++ release.config.js
@@ -0,0 +1,28 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          {
+            type: 'chore',
+            release: 'patch',
+          },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    '@semantic-release/github',
+  ],
+}
